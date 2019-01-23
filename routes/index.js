4a710e3d8b555018d437162aad4413f5788ef04d
mrrohit1 <mrohitmail@gmail.com>
2019-01-23 15:40:05 +0530

    fixed parsing

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var axios = require('axios');
-// const $ = require('cheerio');
-const cheerio = require('cheerio')
+var request = require('request');
+var cheerio = require('cheerio')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	axios.get('https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States')
-		.then(function (response) {
-			// console.log(response);
-			// console.log('dfv', $('.hatnote.navigation-not-searchable', response).length);
-			// console.log($('big > a', response));
-			const $ = cheerio.load(response);
-			const temp = $('big > a');
-			// console.log($('.mediawiki', response));
-			// console.log($('big > a', response));
-			console.log('temp-length: ', temp.length);
-			console.log('temp: ', temp);
-			
-			res.render('index', { title: 'Express' });
-  		})
-  		.catch(function (error) {
-			console.log(error);
+	console.log('start');
+	request('https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States', function (error, response, html) {
+	  if (!error && response.statusCode == 200) {
+		var $ = cheerio.load(html);
+		$('body #content .wikitable td').each(function(element) {
+			var a = $(this).prev();
+			console.log(a.text());
 		});
+		res.render('index', { title: 'Express' });
+	  }
+	});
+	
 });
 
 module.exports = router;
